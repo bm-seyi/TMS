f03325ec1efd436a95bde37cbce8f7f844303b84
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-04-21 18:12:19 +0100

    0.1.4

--- API/Models/Data/LinesModel.cs
+++ API/Models/Data/LinesModel.cs
@@ -2,8 +2,9 @@ namespace TMS_API.Models.Data
 {
     public class LinesModel
     {
-        public Guid Id {get; set;}
-        public double Latitude {get; set;}
-        public double Longitude {get; set;}
+        public Guid Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
     }
 }
--- API/Utilities/DatabaseActions.cs
+++ API/Utilities/DatabaseActions.cs
@@ -2,15 +2,12 @@ using Microsoft.Data.SqlClient;
 using System.Data;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
-using System.Xml.Linq;
-using TMS_API.Models.Data;
 
 namespace TMS_API.Utilities
 {
     public interface IDatabaseActions
     {
-        Task<List<T>> RetrieveModelAsync<T>(string query, CancellationToken cancellationToken = default) where T : new();
-        Task<object?> ProcessQueueMessageAsync(CancellationToken cancellationToken = default);
+        Task<List<T>> RetrieveModelAsync<T>(string query, bool isStoredProcedure, CancellationToken cancellationToken = default) where T : new();
     }
 
     [ExcludeFromCodeCoverage]
@@ -23,98 +20,46 @@ namespace TMS_API.Utilities
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _databaseConnection = databaseConnection ?? throw new ArgumentNullException(nameof(databaseConnection));
         }
-        
-        public async Task<List<T>> RetrieveModelAsync<T>(string query, CancellationToken cancellationToken = default) where T : new()
+
+        public async Task<List<T>> RetrieveModelAsync<T>(string query, bool isStoredProcedure, CancellationToken cancellationToken = default) where T : new()
         {
             await _databaseConnection.OpenAsync(cancellationToken);
             _logger.LogInformation("Connection to database opened");
-            
+
             List<T> listData = new List<T>();
 
             await using (SqlCommand command = _databaseConnection.CreateSqlCommand(query))
-            await using (SqlDataReader reader = await _databaseConnection.ExecuteReaderAsync(command, cancellationToken))
             {
-                var columnOrdinals = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
-                for (int i = 0; i < reader.FieldCount; i++)
-                    columnOrdinals[reader.GetName(i)] = i;
-                _logger.LogInformation("Column ordinals retrieved");
-
-                var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-
-                while (await reader.ReadAsync(cancellationToken))
+                command.CommandType = isStoredProcedure ? CommandType.StoredProcedure : CommandType.Text;
+                await using (SqlDataReader reader = await _databaseConnection.ExecuteReaderAsync(command, cancellationToken))
                 {
-                    var instance = new T();
+                    var columnOrdinals = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        columnOrdinals[reader.GetName(i)] = i;
+                    _logger.LogInformation("Column ordinals retrieved");
 
-                    foreach (var prop in properties)
+                    var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+                    while (await reader.ReadAsync(cancellationToken))
                     {
-                        if (!columnOrdinals.TryGetValue(prop.Name, out int ordinal) || await reader.IsDBNullAsync(ordinal))
-                            continue;
+                        var instance = new T();
 
-                        var value = reader.GetValue(ordinal);
-                        prop.SetValue(instance, Convert.ChangeType(value, prop.PropertyType));
+                        foreach (var prop in properties)
+                        {
+                            if (!columnOrdinals.TryGetValue(prop.Name, out int ordinal) || await reader.IsDBNullAsync(ordinal))
+                                continue;
 
+                            object value = reader.GetValue(ordinal);
+                            Type propType = Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType;
+                            prop.SetValue(instance, Convert.ChangeType(value, propType));
+
+                        }
+                        listData.Add(instance);
                     }
-                    listData.Add(instance);
+                    _logger.LogInformation("Data retrieved and mapped to model: {0}", typeof(T).Name);
                 }
-                _logger.LogInformation("Data retrieved and mapped to model: {0}", typeof(T).Name);
             }
             return listData;
-        }
-
-        public async Task<object?> ProcessQueueMessageAsync(CancellationToken cancellationToken = default)
-        {
-        
-            await _databaseConnection.OpenAsync(cancellationToken);
-            _logger.LogInformation("Connection to database opened for processing queue message");
-
-            List<Guid> affectedIds = new List<Guid>();
-            using (SqlCommand command = _databaseConnection.CreateSqlCommand("WAITFOR (RECEIVE TOP (1) conversation_handle, message_body FROM DataChangeQueue), TIMEOUT 10000;"))
-            await using (SqlDataReader reader = await _databaseConnection.ExecuteReaderAsync(command, cancellationToken))
-            {
-                if (await reader.ReadAsync(cancellationToken))
-                {
-                    Guid conversationHandle = reader.GetGuid(0);
-                    _logger.LogInformation("Conversation handle retrieved: {0}", conversationHandle);
-                    string? message = await reader.IsDBNullAsync(1, cancellationToken) ? null : reader.GetString(1);
-                    List<LinesModel> linesData = new List<LinesModel>();
-
-                    if (!string.IsNullOrWhiteSpace(message))
-                    {
-                        XDocument xDocument = XDocument.Parse(message);
-                        affectedIds.AddRange(
-                        xDocument.Descendants("AffectedID")
-                                .Select(e => Guid.TryParse(e.Value, out var id) ? id : (Guid?)null)
-                                .OfType<Guid>()
-                        );
-                        _logger.LogInformation("Affected IDs retrieved: {0}", string.Join(", ", affectedIds));
-
-                        if (xDocument.Descendants("OperationType").FirstOrDefault()?.Value == "DELETE")
-                        {
-                            _logger.LogInformation("Delete operation detected, affected IDs: {0}", string.Join(", ", affectedIds));
-                            return affectedIds;
-                        }
-
-                        await using (SqlCommand recordsCommand = _databaseConnection.CreateSqlCommand("SELECT [Id], [Latitude], [Longitude] FROM [dbo].[Lines] WHERE [ID] IN (@ids)"))
-                        {
-                            recordsCommand.Parameters.AddWithValue("@ids", string.Join(",", affectedIds));
-                            linesData = await RetrieveModelAsync<LinesModel>(recordsCommand.CommandText, cancellationToken);
-                            _logger.LogInformation("Lines data retrieved for affected IDs: {0}", string.Join(", ", affectedIds));
-                        }
-
-                    }
-
-                    using (SqlCommand endConversation = _databaseConnection.CreateSqlCommand("END CONVERSATION @conv;"))
-                    {
-                        endConversation.Parameters.AddWithValue("@conv", conversationHandle);
-                        await endConversation.ExecuteNonQueryAsync(cancellationToken);
-                        _logger.LogInformation("Conversation ended: {0}", conversationHandle);
-                    }
-                    
-                    return linesData;
-                }
-            }
-            _logger.LogInformation("No messages in queue");
-            return null;
         }
     }
 }
