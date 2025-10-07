9a4201ab4d8ff5bb786586a241f29e8ab0c73af1
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-10-07 20:47:03 +0100

    Add configuration setup for appsettings and environment variables in Program.cs

--- WorkerService/Program.cs
+++ WorkerService/Program.cs
@@ -4,6 +4,14 @@ using WorkerService.BackgroundServices;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables();
+
+builder.AddServiceDefaults();
+
 builder.Services.AddLogging();
 builder.Services.AddSignalR();
 builder.Services.AddHostedService<LinesWorker>();
