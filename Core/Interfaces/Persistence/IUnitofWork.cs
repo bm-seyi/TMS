85fd8b4f51c21a55a34abef0820df668151e35a7
bm-seyi <137287174+bm-seyi@users.noreply.github.com>
2025-11-11 13:46:50 +0000

    Remove Lines repository property from IUnitOfWork interface

--- Core/Interfaces/Persistence/IUnitofWork.cs
+++ Core/Interfaces/Persistence/IUnitofWork.cs
@@ -4,7 +4,6 @@ namespace Core.Interfaces.Persistence
 {
     public interface IUnitofWork : IAsyncDisposable
     {
-        IRepository Lines { get; }
         Task OpenAsync(CancellationToken cancellationToken = default);
         Task BeginAsync(CancellationToken cancellationToken = default);
         Task CommitAsync(CancellationToken cancellationToken = default);
